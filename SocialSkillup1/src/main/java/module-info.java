de47e11a5dd3237ff074a49548e710423ec1e0ad
hannachung01 <87006271+hannachung01@users.noreply.github.com>
2023-05-01 08:38:20 +0300

    Delete SocialSkillup1 directory

--- SocialSkillup1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.socialskillup1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.socialskillup1 to javafx.fxml;
-    exports com.example.socialskillup1;
-}
